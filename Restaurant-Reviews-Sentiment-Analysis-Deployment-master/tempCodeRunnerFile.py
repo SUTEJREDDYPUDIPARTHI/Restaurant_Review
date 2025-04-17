802f47792e40d894954da336e6013209d3a27096
SUTEJREDDYPUDIPARTHI <139585194+SUTEJREDDYPUDIPARTHI@users.noreply.github.com>
2025-04-17 15:07:46 +0530

    Delete Restaurant-Reviews-Sentiment-Analysis-Deployment-master directory

--- Restaurant-Reviews-Sentiment-Analysis-Deployment-master/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-cv = pickle.load(open('C:\Users\LENOVO\Downloads\Restaurant-Reviews-Sentiment-Analysis-Deployment-master\Restaurant-Reviews-Sentiment-Analysis-Deployment-master\cv-transform.pkl','rb'))
