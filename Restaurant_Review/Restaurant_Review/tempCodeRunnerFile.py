c3fcebf1ae683a535721e0be3968c259c275df7a
SUTEJREDDYPUDIPARTHI <139585194+SUTEJREDDYPUDIPARTHI@users.noreply.github.com>
2025-04-17 15:28:00 +0530

    Add files via upload

---
+++ Restaurant_Review/Restaurant_Review/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+cv = pickle.load(open('C:\Users\LENOVO\Downloads\Restaurant-Reviews-Sentiment-Analysis-Deployment-master\Restaurant-Reviews-Sentiment-Analysis-Deployment-master\cv-transform.pkl','rb'))
